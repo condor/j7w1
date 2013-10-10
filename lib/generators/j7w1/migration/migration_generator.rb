af461692acb2e4d3f43d8b8025c61c72d6a2d57c
condor <toyodan@r.recruit.co.jp>
2013-10-10 18:28:57 +0900

    Fixed.

--- lib/generators/j7w1/migration/migration_generator.rb
+++ lib/generators/j7w1/migration/migration_generator.rb
@@ -1,10 +1,27 @@
+require 'rails/generators'
+require 'rails/generator/migration'
+
 module J7W1
   class MigrationGenerator < Rails::Generators::Base
+    include Rails::Generators::Migration
+
     desc "This generator provides the tables which the J7W1 uses."
-    def create_migration_file
+
+    def self.next_migration_number(dirname)
+      if ActiveRecord::Base.timestamped_migrations
+        migration_number = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
+        migration_number += 1
+        migration_number.to_s
+      else
+        "%.3d" % (current_migration_number(dirname) + 1)
+      end
     end
 
-    def create_initializer_file
+    def create_migration_file
+      if self.class.orm_has_migration?
+        migration_template 'migration.rb', 'db/migrate/j7w1_application_devices'
+      end
     end
+    
   end
 end
--- lib/j7w1/rails.rb
+++ lib/j7w1/rails.rb
@@ -1,3 +1,2 @@
 require 'rails'
 require 'j7w1'
-require 'j7w1/generators'
