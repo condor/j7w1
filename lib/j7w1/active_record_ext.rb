19b69980203193e0b2105f04ffd47e234cd9e555
condor <toyodan@r.recruit.co.jp>
2013-10-10 16:01:04 +0900

    Refine APIs.

--- lib/j7w1/active_record_ext.rb
+++ lib/j7w1/active_record_ext.rb
@@ -6,32 +6,32 @@ module J7W1::Sns
 
     module ClassMethods
       private
-      def push_recipient
+      def terminal_owner
         include InstanceMethods
 
-        has_one :sns_endpoint, as: :recipient
+        has_many :application_devices, class_name: 'J7W1::ApplicationDevice'
       end
     end
 
     module InstanceMethods
-      def device_token_updated
-        if device_token
-          update_sns_endpoint
-        else
-          remove_sns_endpoint
+      def add_device(device_identifier, platform)
+        device =
+          devices.where(device_identifier: device_identifier, platform: platform).
+          find_or_initialize
+        device.endpoint_arn = nil
+        device.save!
+      end
+
+      def remove_device(device_identifier, platform)
+        devices.where(device_identifier: device_identifier, platform: platform).
+          destroy_all
+      end
+
+      def push!(options = {})
+        sns_client = self.create_sns_client
+        aplication_devices.each do |device|
+          device.push! sns_client, *options
         end
-      end
-
-      def remove_sns_endpoint
-        sns_endpoint.destroy if sns_endpoint
-      end
-
-      def update_sns_endpoint
-        return if sns_endpoint && sns_endpoint.device_token == device_token
-
-        self.sns_endpoint ||= build_sns_endpoint
-        sns_endpoint.device_token = device_token
-        sns_endpoint.save!
       end
     end
   end
