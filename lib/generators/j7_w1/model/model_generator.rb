70342a7de5a4c2fc0d8e717dd49ce5c0196aa9c3
condor <toyodan@r.recruit.co.jp>
2013-10-10 20:02:24 +0900

    Fix up template location.

--- lib/generators/j7_w1/model/model_generator.rb
+++ lib/generators/j7_w1/model/model_generator.rb
@@ -3,9 +3,18 @@ require 'rails/generators'
 module J7W1
   class ModelGenerator < Rails::Generators::Base
     source_root File.join(File.dirname(__FILE__), 'templates')
+    class_option :async_engine, type: :string, default: nil,
+      desc: 'AWS register/deregister processing method'
 
     def create_model_file
-      create_file "app/models/j7_w1_application_device.rb"
+p methods.sort - Object.methods
+p options
+
+      #create_file "app/models/j7_w1_application_device.rb"
+      #begin
+      #  invoke "j7_w1:migration"
+      #rescue
+      #end
     end
   end
 end
