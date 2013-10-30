b6f2e90317b794a23347f2be2bb3c4c4d7b34db6
condor <toyodan@r.recruit.co.jp>
2013-10-30 23:01:25 +0900

    Add index of the table.

--- lib/generators/j7_w1/migration/templates/active_record/migration.rb
+++ lib/generators/j7_w1/migration/templates/active_record/migration.rb
@@ -6,6 +6,9 @@ class J7W1ApplicationDevices < ActiveRecord::Migration
       t.string  :device_identifier,   null: false
       t.string  :platform,            null: false
       t.string  :device_endpoint_arn, null: true
+
+      t.index [:owner_type, :owner_id]
+      t.index [:device_identifier]
     end
   end
 end
