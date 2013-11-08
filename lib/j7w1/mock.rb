ee1e99a390dc89c266d0be0961954696d484f1f5
condor <toyodan@r.recruit.co.jp>
2013-11-08 20:35:38 +0900

    Mocking add_device_endpoint.

--- lib/j7w1/mock.rb
+++ lib/j7w1/mock.rb
@@ -9,6 +9,10 @@ module J7W1
       def push(endpoint_arn, platform, options)
         @push_histories.push(options.merge(device: {platform: platform, endpoint_arn: endpoint_arn}))
       end
+
+      def create_device_endpoint(device_identifier, platform, options = {})
+        [device_identifier, J7W1::Util.normalize_platform(platform)].compact.join('@')
+      end
     end
   end
 end
