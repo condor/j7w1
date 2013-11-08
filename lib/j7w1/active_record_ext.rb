0ffe376d763b3d54bdfd3afbded307a2fc5bd302
condor <toyodan@r.recruit.co.jp>
2013-11-08 20:10:33 +0900

    Add :as

--- lib/j7w1/active_record_ext.rb
+++ lib/j7w1/active_record_ext.rb
@@ -9,7 +9,8 @@ module J7W1
       def application_device_owner
         include InstanceMethods
 
-        has_many :application_devices, class_name: 'J7W1ApplicationDevice'
+        has_many :application_devices, class_name: 'J7W1ApplicationDevice',
+          as: :owner
       end
     end
 
