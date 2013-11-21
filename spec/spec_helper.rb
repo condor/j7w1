b519e05b8fd2f79c82276b20a9164cfb6ed6d336
condor <toyodan@r.recruit.co.jp>
2013-11-21 12:32:48 +0900

    Spec helper ready. cf. http://stackoverflow.com/questions/4398262/setup-rspec-to-test-a-gem-not-rails

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,8 @@
 require 'rspec'
+require 'bundler/setup'
 
+require 'j7w1'
+
+RSpec.configure do |config|
+  # see https://github.com/rspec/rspec-core/blob/master/lib/rspec/core/configuration.rb for more infomation
+end
