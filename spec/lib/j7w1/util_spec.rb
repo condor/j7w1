d22cccca783987f122248026ffe681c4a6fbc4d0
condor <toyodan@r.recruit.co.jp>
2013-11-22 14:24:00 +0900

    Add :normalize_platform.

--- spec/lib/j7w1/util_spec.rb
+++ spec/lib/j7w1/util_spec.rb
@@ -1,11 +1,11 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '../../spec_helper'))
 
 describe J7W1::Util do
+  subject{
+    Object.new.tap{|o|o.extend J7W1::Util }
+  }
 
   describe :symbolize_keys_recursive do
-    subject{
-      Object.new.tap{|o|o.extend J7W1::Util }
-    }
 
     context "should return the hash whose keys are converted to symbol recursively" do
       specify do
@@ -15,6 +15,31 @@ describe J7W1::Util do
       specify do
         expect(subject.symbolize_keys_recursive({'aaa' => 1, :bbb => 2, '3' => {'aaa' => 5, :'bbb' => 10}})).to eql({aaa: 1, bbb: 2, :'3' => {aaa: 5, bbb: 10}})
       end
+    end
+  end
+
+  describe :normalize_platform do
+    context 'can normalize ios-like platform symbol to :ios' do
+
+      shared_examples_for 'into ios' do
+        specify{expect(subject.normalize_platform(platform_string)).to eql(:ios)}
+      end
+
+      context '"iPad OS" is given' do
+        let(:platform_string){'iPad OS'}
+        it_behaves_like 'into ios'
+      end
+
+      context '"iPhone OS" is given' do
+        let(:platform_string){'iPhone OS'}
+        it_behaves_like 'into ios'
+      end
+
+      context '"iOS" is given' do
+        let(:platform_string){'iOS'}
+        it_behaves_like 'into ios'
+      end
+
     end
   end
 end
