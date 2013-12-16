41fd45fee4741e8ed3a5f7a4d5b2dc180a51c8fe
condor <toyodan@r.recruit.co.jp>
2013-12-16 15:36:44 +0900

    Processing update

--- lib/generators/j7_w1/migration/templates/active_record/migration.rb
+++ lib/generators/j7_w1/migration/templates/active_record/migration.rb
@@ -6,6 +6,7 @@ class J7W1ApplicationDevices < ActiveRecord::Migration
       t.string  :device_identifier,   null: false
       t.string  :platform,            null: false
       t.string  :device_endpoint_arn, null: true
+      t.boolean :disabled,            null: false,  default: false
 
       t.index   [:owner_type, :owner_id]
       t.index   [:device_identifier, :platform], unique: true
